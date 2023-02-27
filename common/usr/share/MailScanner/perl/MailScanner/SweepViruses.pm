53c521c7c62bfc4a30e4a88e977cc1c33c7337b1
Shawn Iverson <shawniverson@efa-project.org>
2023-02-27 17:59:49 -0500

    German verbs and some cleanup

--- common/usr/share/MailScanner/perl/MailScanner/SweepViruses.pm
+++ common/usr/share/MailScanner/perl/MailScanner/SweepViruses.pm
@@ -2124,7 +2124,6 @@ sub ProcessAvastOutput {
 }
 
 sub ProcessEsetsOutput {
-  use File::Basename;
 
   my ($line, $infections, $types, $BaseDir, $Name) = @_;
   chomp $line;
@@ -2160,20 +2159,19 @@ sub ProcessEsetsOutput {
 }
 
 sub ProcessEsetsEFSOutput {
-  use File::Basename;
 
   my ($line, $infections, $types, $BaseDir, $Name) = @_;
   chomp $line;
 
   # return if line does not had threat
-  return 0 if $line !~ m/(?:retained|cleaned)/i;
+  return 0 if $line !~ m/(?:retained|cleaned|beibehalten|gesäubert)/i;
 
   my ($a, $b, $c, $d, $e, $f, $g, $h) = split(/,/, $line);
  
   my ($fileuri) = $c;
   my ($threat) = $d;
-  my ($info) = $e;
-  my ($action) = $f;
+  #my ($info) = $e;
+  #my ($action) = $f;
 
   $fileuri =~ s/file:\/\/$BaseDir/\./;
 
